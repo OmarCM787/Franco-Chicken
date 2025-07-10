4d44e5e19a2672f84162fbbe17f0c9071a774423
Omar <anyelocarrion10@gmail.com>
2025-07-09 22:11:15 -0500

    Contenido actualizado 6

--- PaginaInicial/script.js
+++ PaginaInicial/script.js
@@ -1,6 +1,24 @@
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.getElementById("navLinks")
 const links = navLinks.querySelectorAll("a")
+const portada = document.querySelector(".portada-item.active")
+
+const imagenes = [
+    'url("../Imagenes/Buenazo.jpg")',
+    'url("../Imagenes/Brasas (1).jpg")',
+    'url("../Imagenes/ParrillaParados.jpg")',
+    'url("../Imagenes/ParrillaFamiliar.jpg")',
+]
+
+let i = 0;
+
+function imagenPortada() {
+    portada.style.backgroundImage = imagenes[i];
+    i = (i + 1) % imagenes.length
+}
+
+setInterval(imagenPortada,3000);
+imagenPortada();
 
 menuBtn.addEventListener("click", ()=>{
     navLinks.classList.toggle("active");
